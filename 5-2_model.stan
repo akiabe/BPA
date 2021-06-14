388fbfbede14c9ae08c2faf47b3bab8577d646b6
akiabe <62723205+akiabe@users.noreply.github.com>
2021-06-15 05:59:00 +0900

    Add files via upload

--- 5-2_model.stan
+++ 5-2_model.stan
@@ -4,24 +4,26 @@ data {
 }
 
 parameters {
+  vector<lower=0>[T] lambda;
   real<lower=0,upper=10> mean_lambda;
   real<lower=0,upper=10> sigma_proc;
   real<lower=0,upper=100> sigma_obs;
-  vector<lower=0> [T-1] lambda;
   real<lower=0,upper=500> N_est1;
 }
 
 transformed parameters {
   vector<lower=0>[T] N_est;
   N_est[1] = N_est1;
-  for (t in 1:(T-1)) {
-    N_est[t+1] = N_est[t] * lambda[t];
+  for (i in 2:T) {
+    N_est[i] = N_est[i-1] * lambda[i];
   }
 }
 
 model {
   lambda ~ normal(mean_lambda, sigma_proc);
-  y ~ normal(N_est, sigma_obs);
+  for (i in 1:T) {
+    y[i] ~ normal(N_est[i], sigma_obs);
+  }
 }
 
 generated quantities {
--- 5-2_run-model.R
+++ 5-2_run-model.R
@@ -1,11 +1,13 @@
 library(ggplot2)
+library(rstan)
+rstan_options(auto_write=TRUE)
+options(mc.cores=parallel::detectCores())
 
 n.years <- 25
 N1 <- 30
 mean.lambda <- 1.02
 sigma2.lambda <- 0.02
 sigma2.y <- 20
-
 y <- N <- numeric(n.years)
 N[1] <- N1
 lambda <- rnorm(n=n.years-1, mean=mean.lambda, sd=sqrt(sigma2.lambda))
@@ -18,60 +20,43 @@ for (t in 1:n.years) {
   y[t] <- rnorm(n=1, mean=N[t], sd=sqrt(sigma2.y))
 }
 
-year <- list(1:25)
-d <- data.frame(y=y, T=n.years, year=year)
-d
+p <- data.frame(y=y, year=1:25)
+p
 
-data <- list(y=y, T=n.years)
+ggplot(data=p, aes(x=year, y=y)) +
+  geom_point(alpha=0.6, size=0.9) +
+  ylab("Populations size") +
+  scale_y_continuous(limits=c(0, NA))
+
+d <- list(y=y, T=n.years)
+d
 
 fit <- stan(
   file="5-2_model.stan",
-  data=data,
+  data=d,
   seed=1
 )
 
 print(fit)
 
 ms <- rstan::extract(fit)
-d_qua <- t(apply(
-  X=ms$lambda,
+d_qua <- data.frame(t(apply(
+  X=ms$N_est,
   MARGIN=2,
   FUN=quantile,
   prob=c(0.025, 0.5, 0.975)
-))
+)))
+d_qua
+
 colnames(d_qua) <- c('p2.5', 'p50', 'p97.5')
+d_qua$y <- d$y
+d_qua$year <- 1:25
+d_qua
 
-
-
-
-d_qua <- data.frame(d, d_qua)
-head(d_qua, n=3)
-
-
-year
-
-d <- data.frame(y=y, T=)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ggplot(data=d_qua, aes(x=year, y=y)) +
+  geom_point(alpha=0.6, size=0.9) +
+  geom_line(aes(y=p50), size=0.9) +
+  geom_ribbon(aes(ymin=p2.5, ymax=p97.5), alpha=0.3) +
+  ylab("Populations size") +
+  scale_y_continuous(limits=c(0, NA))
 
